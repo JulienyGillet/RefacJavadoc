3b886bb2053b7b9f70ac29d42dc79ec36908ef05
Julien Gillet <Julinksnow@gmail.com>
2018-06-07 19:34:01 +0200

    Estudiant added

---
+++ Estudiant.java
@@ -0,0 +1,22 @@
+public class Estudiant {
+    private static String nom = "";
+    private static String cognoms = "";
+    public static void setNom(String nom) {
+        Estudiant.nom = nom;
+    }
+
+    public static void setCognoms(String cognoms) {
+        Estudiant.cognoms = cognoms;
+    }
+
+    public static void setDni(String dni) {
+        Estudiant.dni = dni;
+    }
+
+    public static void setCurs(int curs) {
+        Estudiant.curs = curs;
+    }
+
+    private static String dni = "";
+    private static int curs = 0;
+}
