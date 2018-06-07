d1c93acf8bd3acee45db1ddd07ffa7d2d5338adb
Julien Gillet <Julinksnow@gmail.com>
2018-06-07 19:33:48 +0200

    Estudiant added

--- Matricula.java
+++ Matricula.java
@@ -1,24 +1,19 @@
 import java.util.Collection;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Matricula {
 
     public long id = 0;
-    private String nom = "";
-    private String cognoms = "";
-    private String dni = "";
-    private int curs = 0;
     private Collection assignatures;
 
     public Matricula(){}
 
     public Matricula(long id, String nom, String cognoms, String dni, int curs, Collection assignatures) {
         this.id = id;
-        this.nom = nom;
-        this.cognoms = cognoms;
-        this.dni = dni;
-        this.curs = curs;
+        Estudiant.setNom(nom);;
+        Estudiant.setCognoms(cognoms);
+        Estudiant.setDni(dni);
+        Estudiant.setCurs(curs);
         this.assignatures = assignatures;
     }
 
